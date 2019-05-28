a6aea7fa151e291d8f56aa613db73771885dad85
Pavel Podkorytov <pod.pavel@gmail.com>
2019-05-28 10:34:58 +0500

    Update bash aliases

--- bash/.config/bash/20-aliases.sh
+++ bash/.config/bash/20-aliases.sh
@@ -7,6 +7,7 @@ alias h="history_grep"
 alias color-selection="zenity --color-selection"
 alias yaml2js="python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)'"
 alias venv-enable="source ./venv/bin/activate"
+alias convert-python-tabs-to-spaces="find -name '*.py' -print | xargs -n 1 sed -i 's/\t/    /g'"
 
 # Mount
 alias mount.iso='sudo mount -t iso9660 -o loop'
